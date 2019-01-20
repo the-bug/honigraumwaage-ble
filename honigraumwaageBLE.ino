8607a13173085f2205821d81f10fbf71c61e5530
the-bug <#w:1G7j~;*#2.BJc8QH>
2019-01-20 10:09:06 +0100

    First try with BTserial
    Does not deal with char array...

--- honigraumwaageBLE.ino
+++ honigraumwaageBLE.ino
@@ -1,3 +1,6 @@
+#include <AltSoftSerial.h>
+#include "HX711.h"
+
 /*
  This example code uses bogde's excellent library: https://github.com/bogde/HX711
  bogde's library is released under a GNU GENERAL PUBLIC LICENSE
@@ -10,8 +13,7 @@
 
 */
 
-#include "HX711.h"
-
+// HX711
 #define calibration_factor -20570 // 20 KG
 
 #define DT  2 
@@ -19,17 +21,30 @@
 
 HX711 scale(DT, SCK);
 
+// BLE
+AltSoftSerial BTserial; 
+char c=' ';
+
 void setup() {
   initScale();
+  
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+    BTserial.begin(9600);  
+    Serial.println("BTserial started at 9600");
 }
 
 void loop() {
   Serial.print("Reading: ");
   Serial.print(getWeight(), 1);
+  sendWeightToViaBLE(getWeight());
   Serial.print(" kg");
   Serial.println();
   delay(200);
 }
+
+
 
 void initScale() {   
   Serial.println("initScale starts");
@@ -42,5 +57,11 @@ void initScale() {
 float getWeight() {
   Serial.println("getWeight starts and returns");
   return  scale.get_units();  
+}
+
+void sendWeightToViaBLE(float weight) {
+  Serial.println("getWeight starts and returns");   
+        c = weight; 
+        BTserial.write(c);
 }
 
