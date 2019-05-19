c730f018f1ed8a3161b879054d80eac148a5befa
Jonas Scholz <jonas.scholz@die-honigwanderer.de>
2019-05-19 18:54:57 +0200

    Added routing and home component

--- SupperMappingFrontend/src/app/app.module.ts
+++ SupperMappingFrontend/src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';      
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SupperMappingModule } from './supper-mapping/supper-mapping.module';
+import { SupperMappingComponent } from './supper-mapping/supper-mapping.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'enter', component: SupperMappingComponent },
+  { path: '', component: HomeComponent },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+     path: '**',
+     redirectTo: '/',
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    ),
     BrowserModule,
     SupperMappingModule,
     BrowserAnimationsModule,
