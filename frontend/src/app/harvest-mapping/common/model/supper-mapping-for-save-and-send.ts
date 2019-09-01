382331e483997982397a786274ab15fc5a98351c
Jonas Scholz <jonas.scholz@die-honigwanderer.de>
2019-09-01 20:43:33 +0200

    added TODO

--- frontend/src/app/harvest-mapping/common/model/supper-mapping-for-save-and-send.ts
+++ frontend/src/app/harvest-mapping/common/model/supper-mapping-for-save-and-send.ts
@@ -1,6 +1,7 @@
 import { Schleuderung } from './schleuderung';
 
 export interface SupperMappingForSaveAndSend  {
+    // TODO add aditional index to sort in overview
     id?: string;
     date?: Date;
     hiveMark: number;
